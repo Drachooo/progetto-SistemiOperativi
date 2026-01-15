491cf588240aa84fba48d81d409262933703fc71
Drago <matteo.drago.vr@gmail.com>
2026-01-15 13:23:38 +0100

    Rimossi sha256_file.c e sha256_string.c, creato sha256_utils contenente le loro funzioni

--- src/sha256_utils.c
+++ src/sha256_utils.c
@@ -4,18 +4,18 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <openssl/sha.h>
+#include "sha256_utils.h" 
 
-void digest_file(const char *filename, uint8_t * hash) {
-
+int digest_file(const char *filename, uint8_t *hash) {
     SHA256_CTX ctx;
     SHA256_Init(&ctx);
 
-    char buffer[32];
-
-    int file = open(filename, O_RDONLY, 0);
+    char buffer[32]; 
+    
+    int file = open(filename, O_RDONLY);
     if (file == -1) {
         printf("File %s does not exist\n", filename);
-        exit(1);
+        return -1;
     }
 
     ssize_t bR;
@@ -23,33 +23,20 @@ void digest_file(const char *filename, uint8_t * hash) {
         // read the file in chunks of 32 characters
         bR = read(file, buffer, 32);
         if (bR > 0) {
-            SHA256_Update(&ctx,(uint8_t *)buffer,bR);
+            SHA256_Update(&ctx, (uint8_t *)buffer, bR);
         } else if (bR < 0) {
             printf("Read failed\n");
-            exit(1);
+            close(file);
+            return -1;             
         }
     } while (bR > 0);
 
     SHA256_Final(hash, &ctx);
-
     close(file);
+    return 0; // Successo
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2) {
-        printf("Usage: %s <file>\n", argv[0]);
-        exit(1);
-    }
-
-    uint8_t hash[32];
-    digest_file(argv[1], hash);
-
-    char char_hash[65];
+void hash_to_string(const uint8_t *hash, char *output_buffer) {
     for(int i = 0; i < 32; i++)
-        sprintf(char_hash + (i * 2), "%02x", hash[i]);
-
-    printf("%s\n", char_hash);
-
-    return 0;
+        sprintf(output_buffer + (i * 2), "%02x", hash[i]);
 }
