756697aa4db0b9c86b8e4e56d19430c7326ad44f
Drago <matteo.drago.vr@gmail.com>
2026-01-20 17:13:30 +0100

    Minor changes in project

---
+++ project/build/CMakeFiles/sha256_utils.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sha256_utils.dir/src/sha256_utils.c.o"
+  "CMakeFiles/sha256_utils.dir/src/sha256_utils.c.o.d"
+  "libsha256_utils.a"
+  "libsha256_utils.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sha256_utils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ project/build/CMakeFiles/sha256_utils.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libsha256_utils.a"
+)
